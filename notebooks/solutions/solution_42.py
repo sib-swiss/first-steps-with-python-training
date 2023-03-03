d25eacb867d8d0bc45c4017de2c5c2de5d5cca5b
Robin Engler <robin.engler@gmail.com>
2023-03-03 18:02:10 +0100

    notebook 04: move part of exercise 4.2 to Additional Exercises

--- notebooks/solutions/solution_42.py
+++ notebooks/solutions/solution_42.py
@@ -35,36 +35,3 @@ def count_files_2(input_dir):
     return sum(
         (1 for x in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, x)))
     )
-
-
-# Optional task: add an optional argument "ignore_hidden" that, when set to
-# "True", will ignore hidden files (i.e. files whose name is starting with
-# a dot, e.g. ".DS_Store")
-
-def count_files(dir_name, ignore_hidden=False):
-    """Counts files present in the input directory.
-    Only files are counted, directories are ignored.
-    """
-    
-    # Initialize file counter.
-    file_count = 0
-
-    # Loop through all files and directories present in the input directory.
-    for f in os.listdir(path=dir_name):
-
-        # Get the absolute path of the file/directory.
-        full_path = os.path.join(dir_name, f)
-
-        # Verify the path corresponds to a file, not a directory.
-        if os.path.isfile(full_path) and not (ignore_hidden and f.startswith(".")):
-            file_count += 1
-
-    return file_count
-
-parent_dir = os.path.dirname(os.getcwd())
-print("File count in [", parent_dir, "]: ", count_files(parent_dir), sep="")
-print(
-    "File count (excluding hidden files) in [", parent_dir, "]: ",
-    count_files(parent_dir, ignore_hidden=True),
-    sep=""
-)
--- notebooks/solutions/solution_43.py
+++ notebooks/solutions/solution_43.py
@@ -1,75 +1,40 @@
 # Exercise 4.3
 
-# 1. What does is_part_of_set do?
-# *******************************
-from exercise_43_module import is_part_of_set
+import os    # Import the os module into the global namespace.
 
-# help(is_part_of_set)
-# "is_part_of_set()" returns True if its argument is a prime number
-# and False otherwise.
+# We re-use the function from exercise 4.2, and add an optional argument
+# "ignore_hidden" that, when set to "True", will ignore hidden files (i.e.
+# files whose name is starting with a dot, e.g. ".DS_Store")
 
-for n in range(10):
-    print(n, "->", is_part_of_set(n))
+def count_files(dir_name, ignore_hidden=False):
+    """Counts files present in the input directory.
+    Only files are counted, directories are ignored.
 
+    Arguments:
+        dir_name: path of directory in which to count files.
+        ignore_hidden: Optional. If set to True, hidden files (files that
+            start with a '.') are ignored from the count.
+    """
+    
+    # Initialize file counter.
+    file_count = 0
 
-# 2 and 3. Use is_part_of_set to get all primes numbers between 2 and 50000
-# *************************************************************************
-from exercise_43_module import is_part_of_set
-from time import time
+    # Loop through all files and directories present in the input directory.
+    for f in os.listdir(path=dir_name):
 
-primes = []
+        # Get the absolute path of the file/directory.
+        full_path = os.path.join(dir_name, f)
 
-# Get the current time, so we can compute elapsed time at the end.
-t0 = time()
-for i in range(50000):
-    if is_part_of_set(i):
-        primes.append(i)
+        # Verify the path corresponds to a file, not a directory.
+        if os.path.isfile(full_path) and not (ignore_hidden and f.startswith(".")):
+            file_count += 1
 
-time_first_algo = time() - t0
-print("it took", time_first_algo, "seconds")
-print("found", len(primes), "prime numbers")
+    return file_count
 
-
-# Optional: devise a more time efficient way of getting the prime numbers
-# ***********************************************************************
-#
-# Principle: rather than testing each number separately, we test the whole
-# set of numbers at once by going over all number and "eliminating" all
-# multiples of that number.
-from time import time
-
-t0 = time()
-
-# Phase1 : initialization
-upperLimit = 50000
-
-# Create a list that contains True for all numbers we want to test.
-# During the algorithm we will set all non-prime numbers to False.
-arePrime = [True] * (upperLimit + 1)
-arePrime[0] = False  # 0 is not prime
-arePrime[1] = False  # 1 is not prime
-
-
-# Phase2 : go through all numbers
-primes2 = []
-
-for i in range(2, upperLimit + 1):  # for each candidate number
-
-    # only do something if that number has not been set as a non-prime before
-    if arePrime[i]:
-        primes2.append(i)
-
-        # then we want to set all multiples of that number as non-prime
-        mult = 2 * i
-
-        # all multiples until the upper limit is reached
-        while mult <= upperLimit:
-            arePrime[mult] = False  # set the multiple to non-prime
-            mult += i  # nest multiple
-
-time_second_algo = time() - t0
-
-print("it took", time() - t0, "seconds")
-print("speedup compared to first algorithm:", time_first_algo / time_second_algo)
-print("found", len(primes2), "prime numbers")
-print("is this list the same as with the first algorithm ?", primes == primes2)
+parent_dir = os.path.dirname(os.getcwd())
+print("File count in [", parent_dir, "]: ", count_files(parent_dir), sep="")
+print(
+    "File count (excluding hidden files) in [", parent_dir, "]: ",
+    count_files(parent_dir, ignore_hidden=True),
+    sep=""
+)
