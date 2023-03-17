a0d2005fc083b8fd9ae9cd653fd82e364f9310b1
Robin Engler <robin.engler@gmail.com>
2023-03-17 14:42:58 +0100

    solution 32: improve comments in solution

--- notebooks/solutions/solution_32.py
+++ notebooks/solutions/solution_32.py
@@ -8,12 +8,14 @@ with open(input_path, mode="r") as f:
     # Read the header line to skip it: we do not want to store that data.
     l = f.readline()
 
-    # loop through all lines in the file:
+    # Loop through all lines in the file:
     for l in f:
         # Remove the '\n' at the end of the line with "strip()", and
         # split the line according to the ';' character
         sl = l.strip().split(";")
 
+        # Add the strain, region and country values to their respective
+        # lists.
         strain.append(sl[0])
         region.append(sl[1])
         country.append(sl[2])
@@ -23,7 +25,7 @@ print(len(strain), "strains")
 print(len(region), "regions")
 print(len(country), "countries")
 
-# the first 3 entries of each lists:
+# Print the first 3 entries of each lists:
 print("strains   :", strain[:3])
 print("regions   :", region[:3])
 print("countries :", country[:3])
@@ -43,7 +45,7 @@ print(len(strain), "strains")
 print(len(region), "regions")
 print(len(country), "countries")
 
-# the first 3 entries of each lists:
+# Print the first 3 entries of each lists:
 print("strains   :", strain[:3])
 print("regions   :", region[:3])
 print("countries :", country[:3])
