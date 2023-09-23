80c635948f582197f4dfa63a967a4e03ee3d3826
Robin Engler <robin.engler@gmail.com>
2023-09-23 15:00:19 +0200

    notebook 2: minor changes, switch order of exercises 2.3 and 2.4

--- notebooks/solutions/solution_24.py
+++ notebooks/solutions/solution_24.py
@@ -1,75 +1,86 @@
-
-quote = (
-    "Brave Sir Robin ran away. Bravely ran away away. "
-    "When danger reared it's ugly head, he bravely turned his tail and fled. "
-    "Brave Sir Robin turned about and gallantly he chickened out..."
-)
-
-# Instantiate an empty dictionary.
-letter_counts = {}
-
-# Loop through the quote, one character at a time.
-for char in quote:
-
-    # Test whether the current character is already present in the dictionary.
-    if not char in letter_counts:
-        # If not, initialize its count value to 0.
-        letter_counts[char] = 0
-
-    # Increment the number of times the letter has been seen in the quote.
-    letter_counts[char] += 1
+# In this solution, we provide 2 different ways to write a function
+# that creates the reverse complement of a sequence.
 
 
-# Let's now print the results by looping over each key in the dictionary.
-# We additionally sort the output in alphabetical order.
-for letter in sorted(letter_counts):
-    print(letter, letter_counts[letter], sep=" -> ")
+def reverse_complement(seq):
+    """Returns the reverse complement of a sequence given as argument.
+
+    This is the implementation using a dictionary to lookup nucleotide
+    complements.
+    """
+    nucleotide_complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
+
+    # Create an empty string variable that will be used to store the
+    # function's output.
+    reversed_complement = ""
+
+    # Loop through all nucleotides in the sequence in reverse sequence.
+    # In this way, we won't need to reverse the sequence later.
+    for nucleotide in seq[::-1]:
+        # Find the complement of the current nucleotide.
+        reversed_complement += nucleotide_complements[nucleotide]
+
+    # Return the reverse complement.
+    return reversed_complement
 
 
-# Bonus: iterating over key value pairs in a dictionary can be elegantly
-# achieved using a dictionary's "items()" method:
-for letter, count in letter_counts.items():
-    print("'", letter, "' is present ", count, " times", sep="")
+def reverse_complement_2(seq):
+    """Returns the reverse complement of a sequence given as argument.
+
+    This is the implementation using if/else lookup nucleotide complements.
+    """
+    # Create an empty string variable that will be used to store the
+    # function's output.
+    complement = ""
+    # Loop through all nucleotides in the sequence...
+    for nucleotide in seq:
+        if nucleotide == "A":
+            complement += "T"
+        elif nucleotide == "T":
+            complement += "A"
+        elif nucleotide == "G":
+            complement += "C"
+        elif nucleotide == "C":
+            complement += "G"
+        else:
+            # In case the nucleotide is not A, T, G, or C -> error!
+            print("Unknown nucleotide :", nucleotide)
+            print("Abort!")
+            return None
+
+    # Reverse the complement.
+    return complement[::-1]
 
 
-# Implementation to keep only letters and ignore upper/lower cases
-# ****************************************************************
-letter_counts = {}
-for char in quote.lower():
+# Let's test our functions:
+input_seq = "ATAGAGCGATCGATCCCTAGCTA"
+revcomp_seq = reverse_complement(input_seq)
+revcomp_seq_2 = reverse_complement_2(input_seq)
+print("original              :", input_seq)
+print("reverse complemented  :", revcomp_seq)
+print("reverse complemented 2:", revcomp_seq_2)
 
-    # If the current character is not alphabetical (a letter), we
-    # go straight to the next using the "continue" keyword.
-    if not char.isalpha():
-        continue
-
-    # Test whether the current letter is already present in the dictionary.
-    if not char in letter_counts:
-        # If not, initialize its count value to 0.
-        letter_counts[char] = 0
-
-    # Increment the number of times the letter has been seen in the quote.
-    letter_counts[char] += 1
+# Check against output of the online tool:
+online_result = "TAGCTAGGGATCGATCGCTCTAT"
+print("Is 'reverse_complement' result correct?", revcomp_seq == online_result)
+print("Is 'reverse_complement_2' result correct?", revcomp_seq_2 == online_result)
 
 
-
-# Alternative implementation, using the "count()" method of str
-# *************************************************************
-letter_counts = {}
-for char in quote.lower():
-    if char.isalpha() and char not in letter_counts:
-        letter_counts[char] = quote.lower().count(char)
-
-for letter in sorted(letter_counts):
-    print(letter, letter_counts[letter], sep=" -> ")
+# Bonus: shorter way of writing the function.
+def reverse_complement_3(seq):
+    nucleotide_complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    return "".join([nucleotide_complements[n] for n in seq])[::-1]
 
 
-# Bonus: one liner, using set() and dictionary comprehension
-# **********************************************************
-# Note: set(quote) creates a "list" of unique characters in "quote".
-letter_counts = {x: quote.count(x) for x in set(quote)}
+# Bonus: benchmarking of the 3 functions.
+#        Uncomment the code below to run (it takes a little while to run).
+# Warning: %timeit is an iPython "magic" functions that benchmarks a function.
+#          This will only run a Jupyter notebook, not in a regular python shell.
 
-# Same as above, but counting only letters, ignoring upper/lower case
-# differences, and in addition sorting the output alphabetically:
-letter_counts = {x:quote.lower().count(x) for x in sorted(set(quote.lower())) if x.isalpha()}
-for letter in sorted(letter_counts):
-    print(letter, letter_counts[letter], sep=" -> ")
+# test_sequence = "ATAGAGCGATCGATCCCTAG" * 10000
+# print("Benchmarking reverse_complement ...")
+# %timeit reverse_complement(test_sequence)
+# print("Benchmarking reverse_complement_2 ...")
+# %timeit reverse_complement_2(test_sequence)
+# print("Benchmarking reverse_complement_3 ...")
+# %timeit reverse_complement_3(test_sequence)
