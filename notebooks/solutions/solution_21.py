90c65396c524c2e4877e87790a5cba37281fcacd
Robin Engler <robin.engler@gmail.com>
2023-09-27 08:55:49 +0200

    solutions: minor refactoring

--- notebooks/solutions/solution_21.py
+++ notebooks/solutions/solution_21.py
@@ -1,6 +1,8 @@
 threshold = 0.01
-pvNumber = 0
+pvalue_counter = 0
+
 for pv in pvalue_list:
     if pv < threshold:
-        pvNumber += 1
-print("number of pv-values under",threshold,":",pvNumber)
+        pvalue_counter += 1
+
+print("Number of p-values under ", threshold, ": ", pvalue_counter, sep="")
