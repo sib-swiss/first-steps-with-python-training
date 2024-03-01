d665275e977230aa36794d5e88445dfb51d1e67e
Robin Engler <robin.engler@gmail.com>
2024-03-01 15:47:56 +0100

    solutions: cosmetic changes to solutions 52 and 53

--- notebooks/solutions/solution_52.py
+++ notebooks/solutions/solution_52.py
@@ -1,29 +1,24 @@
-
 import pandas as pd
 
-# 1. Read input files as data frames
-# **********************************
+# 1. Read input files as data frames.
 df_hosp = pd.read_csv("data/switzerland_covid19_hospitalized.csv")
 df_demo = pd.read_csv("data/switzerland_demographics.csv")
 
 
-# 2. Update the index of both DataFrames
-# **************************************
+# 2. Update the index of both DataFrames.
 df_hosp.index = df_hosp["Date"]
 df_demo.index = df_demo["Canton"]
 df_demo.head()
 df_hosp.head()
 
 
-# 3. Subset the number of hospitalized person data frame
-# ******************************************************
-# Keep only columns "AG" -> "CH" (i.e. the first 27 columns)
+# 3. Subset the number of hospitalized person data frame.
+# Keep only columns "AG" -> "CH" (i.e. the first 27 columns).
 df_hosp = df_hosp.loc[:, "AG":"CH"]
 df_hosp.head()
 
 
 # 4. Extract population count per Canton
-# **************************************
 pop_by_canton = df_demo.loc[df_hosp.columns, "Population"]
 
 # Note: as long as the index of the Series by which we divide (step 5 below)
@@ -34,29 +29,21 @@ pop_by_canton = df_demo.loc[df_hosp.columns, "Population"]
 
 
 # 5. Compute hospitalized people relative to population size
-# **********************************************************
-# Convert the number of hospitalized people from raw count to 
+# Convert the number of hospitalized people from raw count to
 # number per 10'000 inhabitants.
-df_hosp /= (pop_by_canton / 10000)
+df_hosp /= pop_by_canton / 10000
 df_hosp.head()
 
 
-# 6. For each canton, date at which the hospitalization rate was maximal
-# ***********************************************************************
+# 6. For each canton, date at which the hospitalization rate was maximal.
 # one liner answer:
 df_hosp.idxmax(axis=0)
 
 
-# Additional tasks 
+# Additional tasks
 # ****************
-# Create new data frame with max hospitalized people and date
-df_max_hosp = pd.DataFrame(
-    {
-        "MaxHosp": df_hosp.max(),
-        "Date": df_hosp.idxmax(axis=0)
-        
-    }
-)
+# Create new data frame with max hospitalized people and date.
+df_max_hosp = pd.DataFrame({"MaxHosp": df_hosp.max(), "Date": df_hosp.idxmax(axis=0)})
 
-df_max_hosp.sort_values('Date', ascending=True)
-df_max_hosp.sort_values('MaxHosp', ascending=False)
+df_max_hosp.sort_values("Date", ascending=True)
+df_max_hosp.sort_values("MaxHosp", ascending=False)
--- notebooks/solutions/solution_53.py
+++ notebooks/solutions/solution_53.py
@@ -1,9 +1,7 @@
-
 import pandas as pd
 
-# 1. Center each column: subtract their mean from their values
-# *************************************************************
-df = pd.read_csv("data/mouse_heart_gene_expresssion.tsv", sep="\t")
+# 1. Center each column: subtract their mean from their values.
+df = pd.read_csv("data/mouse_heart_gene_expression.tsv", sep="\t")
 column_means = df.mean()
 print(column_means)
 
@@ -12,7 +10,6 @@ df.head()
 
 
 # 2. select genes whose expression is above the column-wise average
-# *****************************************************************
 # Here we can take advantage from the fact that we have already removed
 # the column averages, so we are interested in positive values.
 mask = (
@@ -25,7 +22,7 @@ df[mask].head()
 
 
 # If we hadn't done the subtraction, here's how we could do it:
-df = pd.read_csv("data/mouse_heart_gene_expresssion.tsv", sep="\t")
+df = pd.read_csv("data/mouse_heart_gene_expression.tsv", sep="\t")
 column_means = df.mean()
 mask = (
     (df["Heart_WT_1"] > column_means["Heart_WT_1"])
