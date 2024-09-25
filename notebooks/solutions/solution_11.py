9223c3faf6ef824a189bf10ad41669f97f66a9d5
Robin Engler <robin.engler@gmail.com>
2024-09-25 22:47:00 +0200

    solutions: small update to explanations of solution_11

--- notebooks/solutions/solution_11.py
+++ notebooks/solutions/solution_11.py
@@ -2,9 +2,11 @@
 
 my_string = "0123456789abcdef"
 
-# The 2 is the step/increment between each selected index in the sequence.
+# The third value passed to the slicing operator (in our example the number 2)
+# is the step/increment between each selected index in the sequence.
 # In this case, every 2nd letter is kept.
 print(my_string[0:10:2])
 
-# This allows the following neat trick to reverse a string: have steps of -1
+# Setting a custom step value also allows the following neat trick to reverse
+# a string: have steps of -1.
 print(my_string[::-1])
