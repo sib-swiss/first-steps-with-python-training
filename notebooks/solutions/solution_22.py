381afb0d81ff75832b599eb36a2290cace724237
WandrilleD <wandrille.duchemin@unibas.ch>
2026-07-31 11:32:56 +0200

    changed one exercise to something more biological

--- notebooks/solutions/solution_22.py
+++ notebooks/solutions/solution_22.py
@@ -2,15 +2,14 @@
 
 # 1. Store our favorite quote as a string variable
 # ************************************************
-quote = "Regarde-moi, mon cher, et dis moi quelle espérance pourrait bien me laisser cette protubérance!"
-quote = "Are you suggesting coconuts migrate ?"
+quote = "ATGCCATTAGCGAGATCGATCGAT"
 
 # 2. Replace all spaces by '@' characters in the quote
 # ****************************************************
 # Since "quote" is a string, we look at "help(str)" to search for methods
 # of str objects. We can see that there is a "replace()" method that allows
 # replacing characters within a string.
-modified_quote = quote.replace(" ", "@")
+modified_quote = quote.replace("T", "U")
 print(quote)
 print(modified_quote)
 
@@ -20,5 +19,4 @@ print(modified_quote)
 # back using "@", essentially replacing white spaces with "@".
 # Note that this will also e.g. replace "\t" and "\n", since they are
 # white spaces, unless we explicitly pass " " to split().
-print("@".join(quote.split()))
-print("@".join(quote.split(" ")))  # Avoids replacing "\t" or "\n"
+print("U".join(quote.split('T')))
